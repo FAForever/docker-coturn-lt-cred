76bf887b2884b88688bf9c1351f94957449eb8d4
Brutus5000 <Brutus5000@gmx.net>
2020-04-29 23:39:55 +0200

    Add Travis CI pushing tags to docker registry

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+set -e
+export REPO=faforever/coturn-lt-cred
+
 docker build -t coturn-lt-cred .
+
+if [ -n "${TRAVIS_TAG}" ]; then
+      docker login -u="${DOCKER_USERNAME}" -p "${DOCKER_TOKEN}";
+      docker tag coturn-lt-cred "${REPO}:${TRAVIS_TAG}";
+      docker push "${REPO}";
+else
+    echo "No tag detected, skipping docker push"
+fi
